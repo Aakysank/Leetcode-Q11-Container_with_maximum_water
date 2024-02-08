6b2ee8c1749e78cb747bbad9df37034f2abe0599
Aakysank <aakashjs1993@gmail.com>
2024-02-08 21:34:31 +0530

    Optimal solution

--- container_with_maximum_water/container_with_maximum_water.cpp
+++ container_with_maximum_water/container_with_maximum_water.cpp
@@ -5,7 +5,8 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+//Brute force approach
+/*class Solution {
 public:
     int maxArea(vector<int>& height) {
         //Brute force approach
@@ -32,11 +33,49 @@ public:
 
         return max_area;
     }
+};*/
+
+//Optimal Solution
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        if (height.size() <= 1)
+            return 0;
+
+        int max_area = 0;
+        size_t i = 0;
+        size_t j = height.size() - 1;
+
+       
+        while (i < j)
+        {
+            int length = 0, width = 0, area = 0;
+            if (height[i] < height[j])
+                length = height[i];
+            else
+                length = height[j];
+
+            width = j - i;
+            area = length * width;
+
+            if (max_area < area)
+                max_area = area;
+
+            if (height[j] < height[i])
+                j--;
+            else
+                i++;
+        }
+
+        return max_area;
+    }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution sol;
+    vector<int> heights = { 1,8,6,2,5,4,8,3,7 };
+    std::cout << sol.maxArea(heights);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
